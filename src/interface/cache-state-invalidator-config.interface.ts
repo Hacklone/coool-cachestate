0fa55fc577d35422e51d6b36b216e7a594fc49cb
Barna Toth <toakak@gmail.com>
2024-01-18 13:15:47 +0100

    Fix config interface

--- src/interface/cache-state-invalidator-config.interface.ts
+++ src/interface/cache-state-invalidator-config.interface.ts
@@ -3,30 +3,25 @@ import {
   InvalidatedNotifierCacheKeyGenerator,
   InvalidatedNotifierKey,
 } from './invalidate-notifier.interface';
+import { RequireAtLeastOne } from 'type-fest';
 
-export type CacheStateInvalidatorConfig = {
+export type CacheStateInvalidatorConfig = RequireAtLeastOne<{
   /**
    * @description generates the cache key for the updated notifier
    * @default undefined
    */
   cacheKeyGenerator?: InvalidatedNotifierCacheKeyGenerator;
-} &
-  (
-    {
-      /**
-       * @description When emits the cache is invalidated. If CacheKey is passed then only that cache otherwise all related cache.
-       * @default undefined
-       */
-      invalidatedNotifier: InvalidatedNotifier;
-      invalidatedNotifierKey: never;
-    }
-    |
-    {
-      /**
-       * @description When emits the cache is invalidated. If CacheKey is passed then only that cache otherwise all related cache.
-       * @default undefined
-       */
-      invalidatedNotifierKey: InvalidatedNotifierKey;
-      invalidatedNotifier: never;
-    }
-    );
+
+  /**
+   * @description When emits the cache is invalidated. If CacheKey is passed then only that cache otherwise all related cache.
+   * @default undefined
+   */
+  invalidatedNotifier?: InvalidatedNotifier;
+
+
+  /**
+   * @description When emits the cache is invalidated. If CacheKey is passed then only that cache otherwise all related cache.
+   * @default undefined
+   */
+  invalidatedNotifierKey?: InvalidatedNotifierKey;
+}, 'invalidatedNotifier' | 'invalidatedNotifierKey'>
--- src/interface/cache-state-updater-config.interface.ts
+++ src/interface/cache-state-updater-config.interface.ts
@@ -1,27 +1,24 @@
 import { UpdatedNotifier, UpdatedNotifierCacheKeyGenerator, UpdatedNotifierKey } from './update-notifier.interface';
+import { RequireAtLeastOne } from 'type-fest';
 
-export type CacheStateUpdaterConfig = {
+export type CacheStateUpdaterConfig = RequireAtLeastOne<{
   /**
    * @description generates the cache key for the updated notifier
    * @default undefined
    */
   cacheKeyGenerator?: UpdatedNotifierCacheKeyGenerator;
-} & (
-  {
-    /**
-     * @description When emits the cache is invalidated and updated. If CacheKey is passed then only that cache otherwise all related cache.
-     * @default undefined
-     */
-    updatedNotifier: UpdatedNotifier;
-    updatedNotifierKey: never;
-  }
-  |
-  {
-    /**
-     * @description When emits the cache is invalidated and updated. If CacheKey is passed then only that cache otherwise all related cache.
-     * @default undefined
-     */
-    updatedNotifierKey: UpdatedNotifierKey;
-    updatedNotifier: never;
-  }
-  );
+
+  /**
+   * @description When emits the cache is invalidated and updated. If CacheKey is passed then only that cache otherwise all related cache.
+   * @default undefined
+   */
+  updatedNotifier?: UpdatedNotifier;
+
+
+  /**
+   * @description When emits the cache is invalidated and updated. If CacheKey is passed then only that cache otherwise all related cache.
+   * @default undefined
+   */
+  updatedNotifierKey?: UpdatedNotifierKey;
+
+}, 'updatedNotifier' | 'updatedNotifierKey'>;
